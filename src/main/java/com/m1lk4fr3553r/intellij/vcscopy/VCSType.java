711b4204b2067390d2722d3fbde6bae73b3282f4
Tobias Birth <m1lk4fr3553r@outlook.de>
2020-11-15 03:05:52 +0100

    Fix URL patterns :bug:

--- src/main/java/com/m1lk4fr3553r/intellij/vcscopy/VCSType.java
+++ src/main/java/com/m1lk4fr3553r/intellij/vcscopy/VCSType.java
@@ -1,8 +1,15 @@
 package com.m1lk4fr3553r.intellij.vcscopy;
 
 public enum VCSType {
-  GITHUB("%1$s/blob/%2$s/%3$s#L%4$d", "%1$s/blob/%2$s/%3$s#L%4$d-L%5$d"),
-  GITLAB("%1$s/-/blob/%2$s/%3$s#L%4$d", "%1$s/-/blob/%2$s/%3$s#L%4$d-%5$d");
+  /*
+  1: Prefix
+  2: Commit
+  3: Filepath
+  4: Start line
+  5: End line
+  */
+  GITHUB("%1$s/blob/%2$s%3$s#L%4$d", "%1$s/blob/%2$s%3$s#L%4$d-L%5$d"),
+  GITLAB("%1$s/-/blob/%2$s%3$s#L%4$d", "%1$s/-/blob/%2$s%3$s#L%4$d-%5$d");
 
   public final String singleLinePattern;
   public final String multiLinePattern;
